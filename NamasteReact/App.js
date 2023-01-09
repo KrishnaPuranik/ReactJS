6d3fa1bee45b1469558c32f3fa7f834cc901f1f1
KrishnaPuranik <krishnamoorthi.puranik@gmail.com>
2023-01-09 23:45:00 +0530

    NamasteReact: Update

---
+++ NamasteReact/App.js
@@ -0,0 +1,26 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+      className: "A1 A2",
+    },
+    "Namaste Everyone on ReactJS!"
+  );
+  console.log(heading1);
+  const heading2 = React.createElement(
+    "h2",
+    {
+      id: "title",
+      className: "A1 A2",
+    },
+    "Namaste Everyone on ReactJS!"
+  );
+  const container = React.createElement(
+    "div",
+    {
+        id: "container"
+    },
+    [heading1, heading2]
+  )
+  const root1 = ReactDOM.createRoot(document.getElementById("root"));
+  root1.render(container);
